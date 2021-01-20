40ad7a6eff7291e7e68ea7e8c176a59ff9668b24
jaybaybay7 <jaymaglione7@gmail.com>
2021-01-20 10:17:07 -0800

    testing GPIO library

--- sendMessage.py
+++ sendMessage.py
@@ -1,2 +1,10 @@
 # using the Python GPIO Library
+#https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
 
+import RPi.GPIO as GPIO
+
+#There are two ways of numbering the IO pins on a Raspberry Pi within RPi.GPIO. 
+#The first is using the BOARD numbering system. This refers to the pin numbers on the P1 header of the Raspberry Pi board. 
+GPIO.setmode(GPIO.BOARD)
+
+print(GPIO.getmode())
