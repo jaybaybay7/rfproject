e275d049cf41581072b5394fea491d476c84558f
jaybaybay7 <jaymaglione7@gmail.com>
2021-01-31 18:11:47 -0800

    fixes to sendMessage

--- sendMessage.py
+++ sendMessage.py
@@ -1,30 +1,24 @@
-# using the Python GPIO Library
-#https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
-
-#!/usr/bin/env python
-
-import piVirtualWire.piVirtualWire as piVirtualWire
+mport piVirtualWire.piVirtualWire as piVirtualWire
 import RPi.GPIO as GPIO
 import pigpio
+import time
 
 
-#There are two ways of numbering the IO pins on a Raspberry Pi within RPi.GPIO. 
-#The first is using the BOARD numbering system. This refers to the pin numbers on the P1 header of the Raspberry Pi board. 
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(37,GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(37,GPIO.OUT)
+                 
 
+print('running')
+state = True
 
-pi = pigpio.pi()
-tx = piVirtualWire.tx(pi,27,1000)
+counter = 0
 
-msg = "hi"
+while counter < 10:
+    print(state)
+    GPIO.output(37, state)
+    counter = counter  + 1
+    if counter == 9:
+        state = not state
+        counter = 0
 
-while True:
-    tx.put(msg)
-    tx.waitForReady()
-
-tx.cancel()
-pi.stop()
-
-
-
+GPIO.cleanup()
