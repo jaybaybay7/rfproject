205a952eb9e71b87019900a6cb5f235c3bf1ae55
jaybaybay7 <jaymaglione7@gmail.com>
2021-03-03 19:41:32 -0800

    fixes

--- switch.py
+++ switch.py
@@ -1,15 +1,14 @@
 from rpi_rf import RFDevice
 import RPi.GPIO as GPIO
-import argparse
+
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(40,GPIO.OUT)
+GPIO.setup(38,GPIO.OUT)
 
-parser = argparse.ArgumentParser(description='Receives a decimal code via a 433/315MHz GPIO device')
-args = parser.parse_args()
-rfdevice = RFDevice(args.gpio)
+
+rfdevice = RFDevice(27)
 
 while True:
     if str(rfdevice.rx_code) == str(345):
-        GPIO.output(40,1)
+        GPIO.output(38,1)
