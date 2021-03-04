af81e0152586138a5b9628512f0fba8880d76015
jaybaybay7 <jaymaglione7@gmail.com>
2021-03-03 19:42:37 -0800

    fixing GPIO version

--- switch.py
+++ switch.py
@@ -2,7 +2,7 @@ from rpi_rf import RFDevice
 import RPi.GPIO as GPIO
 
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(38,GPIO.OUT)
 
@@ -11,4 +11,4 @@ rfdevice = RFDevice(27)
 
 while True:
     if str(rfdevice.rx_code) == str(345):
-        GPIO.output(38,1)
+        GPIO.output(20,1)
